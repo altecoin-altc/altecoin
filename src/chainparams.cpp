29d7ae8b62d14d4195dc9b847ab29217234b020e
altecoin-altc <87372685+altecoin-altc@users.noreply.github.com>
2021-08-19 16:04:55 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -269,8 +269,8 @@ public:
             printf("new mainnet genesis nonce: %s\n", new_nonce.c_str());
             printf("new mainnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
         }
-        //assert(hashGenesisBlock == uint256("00000ecff770b2c9991d5a360f0382b2251bd5c645c8a418f8f4d8211badf109"));
-        //assert(genesis.hashMerkleRoot == uint256("d87be81247131bb1a0813cb24851731a0a46d9ae92534067baebba675a800e5b"));
+        assert(hashGenesisBlock == uint256("00000ecff770b2c9991d5a360f0382b2251bd5c645c8a418f8f4d8211badf109"));
+        assert(genesis.hashMerkleRoot == uint256("d87be81247131bb1a0813cb24851731a0a46d9ae92534067baebba675a800e5b"));
 
         //vSeeds.push_back(CDNSSeedData("139.180.156.8", "139.180.156.8"));
 		//vSeeds.push_back(CDNSSeedData("178.62.46.38", "178.62.46.38"));
