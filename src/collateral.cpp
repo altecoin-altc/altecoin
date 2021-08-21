dc77d5e6d2c8bb0ed6a9f7f2e3cf760cdc229587
altecoin-altc <87372685+altecoin-altc@users.noreply.github.com>
2021-08-21 20:57:14 +0800

    Update collateral.cpp
    change collateral amount

--- src/collateral.cpp
+++ src/collateral.cpp
@@ -18,37 +18,7 @@ CAmount CollateralRequired(int nHeight)
 {
     CAmount nCollateralRequired;
     
-    if (nHeight <= 500) {
-        nCollateralRequired = 0 * COIN;
-    } else if (nHeight <= 1000) {
-        nCollateralRequired = 50 * COIN;
-    } else if (nHeight <= 10000) {
-        nCollateralRequired = 100 * COIN;
-    } else if (nHeight <= 20000) {
-        nCollateralRequired = 350 * COIN;
-    } else if (nHeight <= 30000) {
-        nCollateralRequired = 500 * COIN;
-    } else if (nHeight <= 50000) {
-        nCollateralRequired = 750 * COIN;
-    } else if (nHeight <= 60000) {
-        nCollateralRequired = 1250 * COIN;
-    } else if (nHeight <= 90000) {
-        nCollateralRequired = 2500 * COIN;
-    } else if (nHeight <= 120000) {
-        nCollateralRequired = 5000 * COIN;
-    } else if (nHeight <= 150000) {
-        nCollateralRequired = 10000 * COIN;
-    } else if (nHeight <= 200000) {
-        nCollateralRequired = 25000 * COIN;
-    } else if (nHeight <= 250000) {
-        nCollateralRequired = 40000 * COIN;
-    } else if (nHeight <= 300000) {
-        nCollateralRequired = 50000 * COIN;
-    } else if (nHeight <= 400000) {
-        nCollateralRequired = 60000 * COIN;
-    } else {
-        nCollateralRequired = 750000 * COIN;
-    }
+    nCollateralRequired = 5000 * COIN;
 
     return nCollateralRequired;
 }
